21530614431e13bbde8e14eb1c1b1565d6fda48d
Anurag30112003 <anuragsharma011011@gmail.com>
2022-01-12 15:22:29 +0530

    hello

--- 01.py
+++ 01.py
@@ -7,9 +7,11 @@ def check_prime(n):
         if n % i == 0:
             return False
     return True
+
+
 if __name__ == '__main__':
     n = int(input("Enter a number: "))
     if check_prime(n):
-        print(f"{n} is a prime number." )
+        print(f"{n} is a prime number.")
     else:
         print(f"{n} is not a prime number.")
